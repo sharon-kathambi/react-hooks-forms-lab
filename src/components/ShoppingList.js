0194d3fe3160c9025cb7a174953c9b30371c44d3
Sharon Mwenda <smkathambi@gmail.com>
2022-07-20 13:37:20 +0300

    Added search

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,13 +5,29 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchData, setSearchData] = useState("")
+
+  function handleSearchChange(event){
+    setSearchData("");
+    setSearchData(event.target.value)
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  /*const searchDataToDisplay = (items, searchData)=>{
+    if(!searchData){
+      return items;
+    }
+  
+  return items.filter((item) => {
+    item.toLowerCase().includes(searchData)
+  })
+  }*/
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    if(searchData !== "") return item['name'].toLowerCase().includes(searchData);
+     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
@@ -19,7 +35,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onSearchChange={handleSearchChange} onCategoryChange={handleCategoryChange}  />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
